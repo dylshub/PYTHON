f89d867dcb30c9cfc903fcd8f32141695d35b31f
Dylan Rodas <dylanrodas10@gmail.com>
2024-07-06 23:04:38 -0600

    first commit

---
+++ 3CALCULADORAEJEMPLO.py
@@ -0,0 +1,16 @@
+print ("-----CALCULADORA-----")
+
+num1 = (input('ingresa un numero: '))
+num2 = (input('ingresa un numero: '))
+
+res = float(num1) + float(num2)
+res2=float(num1) - float (num2)
+res3=float(num1) * float (num2)
+res4 = float(num1) / float(num2)
+
+print ('La suma es ', res)
+print ('La resta es ', res2)
+print ('La multiplicacion es ', res3)
+print ('La division es ', res4)
+
+print ("--------------------")
